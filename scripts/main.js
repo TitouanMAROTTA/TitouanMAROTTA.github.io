9c214ce25ad99dcc20fbc72a93497a8eff94d066
TitouanMAROTTA <90621409+TitouanMAROTTA@users.noreply.github.com>
2023-06-24 03:08:33 +0200

    edit buttons : remove focus on click

--- scripts/main.js
+++ scripts/main.js
@@ -4,4 +4,16 @@ AOS.init({
   duration: 1000
 });
 
+
+// Remove focus from the clicked button
+
+  let buttons = document.querySelectorAll('.btn');
+
+  buttons.forEach(function(button){
+    button.addEventListener('click', function() {
+      this.blur(); // Remove focus from the clicked button
+    });
+  });
+
+  
 // Add your javascript here
